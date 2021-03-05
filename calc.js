2415eb6c2ddb84b47608d5892f49c437fd21ad6e
Raumil Thakkar <raumilthakkar@gmail.com>
2021-03-05 14:50:04 -0500

    calc

---
+++ calc.js
@@ -0,0 +1,111 @@
+// let oneBtn=document.getElementById('one');
+// let twoBtn=document.getElementById('two');
+// let threeBtn=document.getElementById('three');
+// let fourBtn=document.getElementById('four');
+// let fiveBtn=document.getElementById('five');
+// let sixBtn=document.getElementById('six');
+// let sevenBtn=document.getElementById('seven');
+// let eigthBtn=document.getElementById('eight');
+// let nineBtn=document.getElementById('nine');
+// let zeroBtn=document.getElementById('zero');
+let calcNumBtns = document.getElementsByClassName('num')
+let calcOpeBtns = document.getElementsByClassName('ope')
+
+let decimalBtn = document.getElementById('decimal');
+let clearBtn = document.getElementById('clear');
+let backBtn = document.getElementById('backspace');
+let displayValElement = document.getElementById('display-val')
+let eq = document.getElementById('equals')
+let inv=document.getElementById('invert')
+
+let displayVal = '0';
+let pendingVal = 0;
+
+let opp;
+
+function updateDisplayVal(clickObj) {
+    let btnText = clickObj.target.innerText;
+    if (displayVal === '0')
+        displayVal = '';
+
+    displayVal += btnText;
+    displayValElement.innerText = displayVal;
+
+console.log(displayVal);
+}
+let ope = ''
+
+const perform = (clickObj) => {
+    ope = clickObj.target.innerText;
+
+    pendingVal += +displayVal;
+    // displayValElement.innerText = '0';
+    displayVal = '';
+
+}
+let ans;
+eq.onclick = (e) => {
+    displayVal = displayValElement
+    if (ope === '+') {
+        ans = +pendingVal + +displayValElement.innerText;
+        displayValElement.innerText = ans;
+        console.log(ans);
+    }
+    else if (ope === '-') {
+        ans = +pendingVal - +displayValElement.innerText;
+        displayValElement.innerText=ans;
+        console.log(ans);
+    }
+    else if (ope === 'x') {
+        ans = +pendingVal * +displayValElement.innerText;
+        displayValElement.innerText=ans;
+        console.log(ans);
+    }
+    else if (ope === '÷') {
+        ans = +pendingVal / +displayValElement.innerText;
+        displayValElement.innerText= +ans;
+        console.log(ans);
+    }
+    if (ope==='%'){
+        if (displayValElement==0){
+            // ans=;
+        }
+        ans = +pendingVal / +displayValElement.innerText;
+        
+    }
+
+}
+
+
+for (let i = 0; i < calcNumBtns.length; i++) {
+    calcNumBtns[i].addEventListener('click', updateDisplayVal, false);
+}
+for (i = 0; i < calcOpeBtns.length; i++) {
+    calcOpeBtns[i].addEventListener('click', perform, false);
+    // console.log(calcOpeBtns);
+}
+clearBtn.onclick = () => {
+    displayVal = '0';
+    pendingVal = 0;
+
+    displayValElement.innerText = displayVal;
+
+}
+backBtn.onclick = () => {
+    let lengthOfDisplayVal = displayVal.length;
+    displayVal = displayVal.slice(0, lengthOfDisplayVal - 1);
+    if (displayVal === '')
+        displayVal = '0';
+
+    displayValElement.innerText = displayVal;
+}
+decimalBtn.onclick = () => {
+    if (!displayVal.includes('.')){
+        displayVal += '.';
+        displayValElement.innerText = displayVal;
+    }
+}
+inv.onclick=()=>{
+        displayValElement.innerText = displayValElement.innerText*(-1);
+}
+
